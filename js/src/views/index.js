94e12866987b70261766878513c72a93b1ee780d
Eirik S. Morland <eirik@morland.no>
2014-10-04 14:44:47 +0200

    Fix error when having 0 nodes, fix problematic m.route links.

--- js/src/views/index.js
+++ js/src/views/index.js
@@ -3,18 +3,19 @@
 module.exports = view;
 
 function view(ctrl) {
+  var urlPrefix = window.location.host;
   var renderListItem = function(item) {
     return m("article", {class:"node node--view-mode-teaser clearfix", about:"/aa-cc", typeof:"schema:Article"}, [
       m("h2", {class:"node__title " }, [m("a", {href:item.path, config:m.route}, [item.title])]),
       m("div", {class:"node__meta"}, [
         "Submitted ", item.created
       ]),
-      m("div", {class:"read-more"}, [m("a", {href:item.path, config:m.route}, ["Read more"])]),
+      m("div", {class:"read-more"}, [m("a", {href:'//' + urlPrefix + item.path, config:m.route}, ["Read more"])]),
       m("div", {class:"comment-count"}, [m("a", {'data-disqus-identifier':item.path, href:item.path, config:m.route})]),
       m.trust(item.field_tags)
     ])
   }
   return m("div", {class:"content"}, [
-    m("div", [ ctrl.nodes.map(renderListItem) ])
+    m("div", [ ctrl.nodes.map ? ctrl.nodes.map(renderListItem) : ''])
   ])
 }
--- js/src/views/jsx/index.jsx
+++ js/src/views/jsx/index.jsx
@@ -3,18 +3,19 @@
 module.exports = view;
 
 function view(ctrl) {
+  var urlPrefix = window.location.host;
   var renderListItem = function(item) {
     return <article class="node node--view-mode-teaser clearfix" about="/aa-cc" typeof="schema:Article">
       <h2 class="node__title "><a href={item.path} config={m.route}>{item.title}</a></h2>
       <div class="node__meta">
         Submitted {item.created}
       </div>
-      <div class="read-more"><a href={item.path} config={m.route}>Read more</a></div>
+      <div class="read-more"><a href={'//' + urlPrefix + item.path} config={m.route}>Read more</a></div>
       <div class="comment-count"><a data-disqus-identifier={item.path} href={item.path} config={m.route}></a></div>
       {m.trust(item.field_tags)}
     </article>
   }
   return <div class="content">
-    <div>{ ctrl.nodes.map(renderListItem) }</div>
+    <div>{ ctrl.nodes.map ? ctrl.nodes.map(renderListItem) : ''}</div>
   </div>
 }
