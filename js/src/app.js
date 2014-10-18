49e3a732bb4a26463a7a3323a7da0e2f8c8f77af
Eirik S. Morland <eirik@morland.no>
2014-10-18 16:46:33 +0200

    Do disqus even if app is not inited.

--- js/src/app.js
+++ js/src/app.js
@@ -1,6 +1,7 @@
 var m = require('mithril');
 
 var Booter = require('./booter');
+var disqus = require('./disqus');
 
 var node = require('./controllers/node');
 var Index = require('./controllers/index');
@@ -53,6 +54,7 @@ function init(window) {
   };
   app.booter = new Booter(app);
   app.booter.attach();
+  disqus(app.window);
   // Try to find comment count.
   window.disqus_shortname = '{{ disqus_shortname }}';
   // That is... unless there is no disqus set up for this site.
